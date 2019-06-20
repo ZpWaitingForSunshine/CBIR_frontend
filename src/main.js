f76186e469c3fbd5591bb277a0b7d7b3115a3025
ZpWaitingForSunshine <penn0121@163.com>
2019-06-20 18:18:42 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,52 +1,51 @@
-import babelpolyfill from 'babel-polyfill'
 import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+import '@/styles/index.scss' // global css
+
 import App from './App'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-//import './assets/theme/theme-green/index.css'
-import VueRouter from 'vue-router'
-import store from './vuex/store'
-import Vuex from 'vuex'
-//import NProgress from 'nprogress'
-//import 'nprogress/nprogress.css'
-import routes from './routes'
-import Mock from './mock'
-Mock.bootstrap();
-import 'font-awesome/css/font-awesome.min.css'
+import store from './store'
+import router from './router'
 
-Vue.use(ElementUI)
-Vue.use(VueRouter)
-Vue.use(Vuex)
+import './icons' // icon
+import './permission' // permission control
+import './utils/error-log' // error log
 
-//NProgress.configure({ showSpinner: false });
+import * as filters from './filters' // global filters
 
-const router = new VueRouter({
-  routes
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
 })
 
-router.beforeEach((to, from, next) => {
-  //NProgress.start();
-  if (to.path == '/login') {
-    sessionStorage.removeItem('user');
-  }
-  let user = JSON.parse(sessionStorage.getItem('user'));
-  if (!user && to.path != '/login') {
-    next({ path: '/login' })
-  } else {
-    next()
-  }
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
 })
 
-//router.afterEach(transition => {
-//NProgress.done();
-//});
+Vue.config.productionTip = false
 
 new Vue({
-  //el: '#app',
-  //template: '<App/>',
+  el: '#app',
   router,
   store,
-  //components: { App }
   render: h => h(App)
-}).$mount('#app')
-
+})
